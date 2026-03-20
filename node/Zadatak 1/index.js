7ca63da12ddf497b86a17bdc6ed753d44084a524
aleksandar <acodex00@gmail.com>
2026-03-20 13:52:45 +0100

    task requirements added

--- node/Zadatak 1/index.js
+++ node/Zadatak 1/index.js
@@ -1,5 +1,8 @@
 const users = require("./users");
 
+// Zadatak 1:
+// Kreirati listu korisnika u fajlu "users.js", eksportovati ih
+// i ispisati sve korisnike iz niza "users" u konzoli.
 users.forEach((user) => {
 	console.log(user);
 });
