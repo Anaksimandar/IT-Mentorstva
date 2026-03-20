17b3519138c07867ae4ba130ca7858af62dfd61a
aleksandar <acodex00@gmail.com>
2026-03-20 20:09:41 +0100

    Node - Homework 1 done

--- node/Domaci 1/index.js
+++ node/Domaci 1/index.js
@@ -1,14 +1,16 @@
-// Zadatak 2
-// Ispisati na glavnoj stranici "Dobrodosli na glavnu stranicu" (ne u konzoli vec u pregledacu)
-
 const http = require("http");
 
+// Domaci 1:
+// Ako korisnik poseti '/kontakt' stranicu, preusmeriti ga na glavnu stranicu uz odgovarajuci statusni kod.
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader("Content-Type", "text/plain");
 
 	if (req.url === "/") {
 		res.end("Dobrodosli na glavnu stranicu");
+	} else if (req.url === "/kontakt") {
+		res.writeHead(301, { location: "/" });
+		res.end();
 	} else {
 		res.statusCode = 404;
 		res.setHeader("Content-Type", "text/plain");
