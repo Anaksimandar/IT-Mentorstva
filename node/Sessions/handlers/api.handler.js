778c42d96065cfa20e477f833005d691e82a70a0
aleksandar <acodex00@gmail.com>
2026-04-13 14:30:18 +0200

    Sessions lession with tasks completed.

---
+++ node/Sessions/handlers/api.handler.js
@@ -0,0 +1,126 @@
+const { registerUser, loginUser } = require("../services/user.service");
+const querystring = require("querystring");
+const {
+	validateEmail,
+	validatePassword,
+	isPasswordSame,
+	isCheckBoxChecked,
+	validateName,
+} = require("../helper/validator");
+const { createSession } = require("../services/session.service");
+
+const apiHandler = async (req, res) => {
+	if (req.url === "/api/register" && req.method === "POST") {
+		// Handle registration API endpoint
+		let data = "";
+		req.on("data", (chunk) => {
+			data += chunk;
+		});
+
+		req.on("end", async () => {
+			const { name, email, password, confirmPassword, terms } =
+				querystring.parse(data);
+			if (!validateName(name)) {
+				res.statusCode = 400;
+				res.setHeader("Content-Type", "application/json");
+				return res.end(
+					JSON.stringify({
+						message: "Name must be at least 3 characters long",
+					}),
+				);
+			}
+			if (!validateEmail(email)) {
+				res.statusCode = 400;
+				res.setHeader("Content-Type", "application/json");
+				return res.end(JSON.stringify({ message: "Invalid email format" }));
+			}
+			if (!validatePassword(password)) {
+				res.statusCode = 400;
+				res.setHeader("Content-Type", "application/json");
+				return res.end(
+					JSON.stringify({
+						message: "Password must be at least 3 characters long",
+					}),
+				);
+			}
+			if (!isPasswordSame(password, confirmPassword)) {
+				res.statusCode = 400;
+				res.setHeader("Content-Type", "application/json");
+				return res.end(JSON.stringify({ message: "Passwords do not match" }));
+			}
+
+			if (!isCheckBoxChecked(terms)) {
+				res.statusCode = 400;
+				res.setHeader("Content-Type", "application/json");
+				return res.end(
+					JSON.stringify({
+						message: "You must accept the terms and conditions",
+					}),
+				);
+			}
+			try {
+				const userId = await registerUser(name, email, password);
+				const sessionId = createSession(userId);
+				res.statusCode = 200;
+				res.setHeader("Set-Cookie", `sid=${sessionId}; Path=/; HttpOnly`);
+				res.setHeader("Content-Type", "application/json");
+				return res.end(JSON.stringify({ message: "Registration successful" }));
+			} catch (error) {
+				console.error("Error occurred while registering user:", error);
+				res.statusCode = 500;
+				res.setHeader("Content-Type", "application/json");
+				return res.end(JSON.stringify({ message: error.message }));
+			}
+		});
+	} else if (req.url === "/api/login" && req.method === "POST") {
+		// Handle login API endpoint
+		let data = "";
+		req.on("data", (chunk) => {
+			data += chunk;
+		});
+		req.on("end", async () => {
+			const { email, password } = querystring.parse(data);
+			if (!validateEmail(email)) {
+				res.statusCode = 400;
+				res.setHeader("Content-Type", "application/json");
+				return res.end(JSON.stringify({ message: "Invalid email format" }));
+			}
+			if (!validatePassword(password)) {
+				res.statusCode = 400;
+				res.setHeader("Content-Type", "application/json");
+				return res.end(
+					JSON.stringify({
+						message: "Password must be at least 3 characters long",
+					}),
+				);
+			}
+			try {
+				const user = await loginUser(email, password);
+				if (user) {
+					const sessionId = createSession(user.id);
+					res.statusCode = 200;
+					res.setHeader("Set-Cookie", `sid=${sessionId}; Path=/; HttpOnly`);
+					res.setHeader("Content-Type", "application/json");
+					return res.end(JSON.stringify({ message: "Login successful" }));
+				} else {
+					res.statusCode = 401;
+					res.setHeader("Content-Type", "application/json");
+					return res.end(
+						JSON.stringify({ message: "Invalid email or password" }),
+					);
+				}
+			} catch (error) {
+				console.error("Error occurred while logging in:", error);
+				res.statusCode = 500;
+				res.setHeader("Content-Type", "application/json");
+				return res.end(JSON.stringify({ message: "Internal Server Error" }));
+			}
+		});
+	} else {
+		res.statusCode = 404;
+		res.setHeader("Content-Type", "text/plain");
+		return res.end("API Endpoint Not Found\n");
+	}
+};
+
+module.exports = apiHandler;
